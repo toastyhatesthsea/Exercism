909c0d59c70894bf7dce0468b2ba31d1d8d3f5b5
Saena <toastyhatesthesea@gmail.com>
2020-09-20 04:12:16 -0700

    Added initial code for Piglatin

--- PigLatinTranslator.java
+++ PigLatinTranslator.java
@@ -8,3 +8,36 @@ which is an important part of programming in Java.
 Please remove this comment when submitting your solution.
 
 */
+
+import java.util.Scanner;
+
+public class PigLatinTranslator
+{
+    private String originalWord;
+
+    public PigLatinTranslator()
+    {
+
+    }
+
+    public String translate(String aWordToTranslate)
+    {
+        Scanner aScanOfWord = new Scanner(aWordToTranslate);
+        //TODO Add a way to scan each word and process
+
+        if (aWordToTranslate.length() > 1)
+        {
+            String firstLetter = aWordToTranslate.substring(0, 1);
+
+        }
+
+
+
+        switch (aWordToTranslate)
+        {
+            case
+        }
+    }
+
+}
+
