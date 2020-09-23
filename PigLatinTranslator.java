647570ed3583263f165a66ec85a20de02571a1ad
Saena <toastyhatesthesea@gmail.com>
2020-09-22 17:13:56 -0700

    Added some basic Piglatin methods

--- PigLatinTranslator.java
+++ PigLatinTranslator.java
@@ -25,6 +25,11 @@ public class PigLatinTranslator
         Scanner aScanOfWord = new Scanner(aWordToTranslate);
         //TODO Add a way to scan each word and process
 
+        while (aScanOfWord.hasNext())
+        {
+
+        }
+
         if (aWordToTranslate.length() > 1)
         {
             String firstLetter = aWordToTranslate.substring(0, 1);
@@ -32,10 +37,30 @@ public class PigLatinTranslator
         }
 
 
-
         switch (aWordToTranslate)
         {
             case
+        }
+    }
+
+    public String checkWord(String aWord)
+    {
+
+    }
+
+    public String checkLetter(String aLetter)
+    {
+        switch (aLetter)
+        {
+            case "a":
+            case "e":
+            case "i":
+            case "o":
+            case "u":
+            case "y":
+                return "vowel";
+            default:
+                return "const";
         }
     }
 
