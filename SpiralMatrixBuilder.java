a13a85af6e81de2c43eb09fb825d7866e7092471
Saena <toastyhatesthesea@gmail.com>
2020-11-09 22:07:10 -0800

    Added initial constructor and initial code

---
+++ SpiralMatrixBuilder.java
@@ -0,0 +1,24 @@
+/*
+
+Since this exercise has a difficulty of > 4 it doesn't come
+with any starter implementation.
+This is so that you get to practice creating classes and methods
+which is an important part of programming in Java.
+
+Please remove this comment when submitting your solution.
+
+*/
+
+
+public class SpiralMatrixBuilder
+{
+    public int [][] theMatrix;
+
+
+    public int[][] buildMatrixOfSize(int size)
+    {
+        theMatrix = new int[0][0];
+
+        return theMatrix;
+    }
+}
