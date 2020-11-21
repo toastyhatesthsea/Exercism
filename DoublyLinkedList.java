b077c7b78eadc2348958d7e0d89adcffa448b3b9
Zafaripour <toastyhatesthesea@gmail.com>
2020-11-21 02:59:01 -0800

    Push and Pop completed

--- DoublyLinkedList.java
+++ DoublyLinkedList.java
@@ -10,9 +10,9 @@ Please remove this comment when submitting your solution.
 */
 
 
-public class DoublyLinkedList <E>
+public class DoublyLinkedList<E>
 {
-    public DoubleNode<E> head;
+    public DoubleNode<E> head, tail;
 
 
     public DoublyLinkedList()
@@ -20,10 +20,100 @@ public class DoublyLinkedList <E>
     }
 
 
-
     public void push(E someDataThing)
     {
+        if (head == null)
+        {
+            head = new DoubleNode<>(someDataThing);
+            tail = head;
+        } else
+        {
+            DoubleNode<E> oldHead = head;
+            DoubleNode<E> newHead = new DoubleNode<>(someDataThing);
+            if (head == tail)
+            {
+                newHead.setBackward(head);
+                head = newHead;
+                oldHead.setForward(head);
+                tail = oldHead;
+            } else
+            {
+                newHead.setBackward(oldHead);
+                oldHead.setForward(newHead);
+                head = newHead;
+            }
+        }
+    }
 
+    public void unshift(E someData)
+    {
+        if (head == null)
+        {
+            head = new DoubleNode<>(someData);
+            tail = head;
+        }
+        else
+        {
+            DoubleNode<E> oldTail = tail;
+            DoubleNode<E> newTail = new DoubleNode<>(someData);
+            newTail.setForward(oldTail);
+            oldTail.setBackward(newTail);
+            tail = newTail;
+        }
+    }
+
+    public E shift()
+    {
+        E answer = null;
+
+        if (head == tail)
+        {
+            answer = head.getData();
+            head = null;
+        }
+        else
+        {
+            answer = tail.getData();
+            tail.getForward().setBackward(null);
+            tail = tail.getForward();
+        }
+
+        return answer;
+    }
+
+    public E pop()
+    {
+        E answer = null;
+        if (head == tail)
+        {
+            answer = head.getData();
+            head = null;
+            tail = null;
+        } else
+        {
+            answer = head.getData();
+            head.getBackward().setForward(null);
+            head = head.getBackward();
+
+        }
+
+        return answer;
+    }
+}
+
+class MeowersTestersMeows
+{
+    public static void main(String[] asdasdasdasd)
+    {
+        DoublyLinkedList rawrs = new DoublyLinkedList();
+
+        rawrs.push(12);
+        rawrs.push("Meows");
+        rawrs.push(999999);
+
+        rawrs.pop();
+        rawrs.pop();
+        rawrs.pop();
     }
 }
 
@@ -32,10 +122,10 @@ class DoubleNode<E>
 {
 
     public E data;
-    public DoublyLinkedList<E> forward;
-    public DoublyLinkedList<E> backward;
+    public DoubleNode<E> forward;
+    public DoubleNode<E> backward;
 
-    public DoubleNode(E data, DoublyLinkedList<E> forward, DoublyLinkedList<E> backward)
+    public DoubleNode(E data, DoubleNode<E> forward, DoubleNode<E> backward)
     {
         this.data = data;
         this.forward = forward;
@@ -61,22 +151,22 @@ class DoubleNode<E>
         this.data = data;
     }
 
-    public DoublyLinkedList<E> getForward()
+    public DoubleNode<E> getForward()
     {
         return forward;
     }
 
-    public void setForward(DoublyLinkedList<E> forward)
+    public void setForward(DoubleNode<E> forward)
     {
         this.forward = forward;
     }
 
-    public DoublyLinkedList<E> getBackward()
+    public DoubleNode<E> getBackward()
     {
         return backward;
     }
 
-    public void setBackward(DoublyLinkedList<E> backward)
+    public void setBackward(DoubleNode<E> backward)
     {
         this.backward = backward;
     }
