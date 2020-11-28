093d4c939aa289b9bce2152e89d4d3089390f932
Zafaripour <toastyhatesthesea@gmail.com>
2020-11-28 00:43:38 -0800

    Finished match() function

--- Anagram.java
+++ Anagram.java
@@ -10,6 +10,7 @@ Please remove this comment when submitting your solution.
 */
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
@@ -20,10 +21,9 @@ public class Anagram
 
     public Anagram(String someWord)
     {
-        this.aWord = someWord;
+        this.aWord = someWord.toLowerCase();
 
-        wordKey = processWord(this.aWord);
-
+        //wordKey = processWord(this.aWord);
     }
 
     public String getaWord()
@@ -36,9 +36,36 @@ public class Anagram
         this.aWord = aWord;
     }
 
+    /**
+     * Checks object string for Anagrams. Just simply sorts the strings and see if they line up equally.
+     * Object string cannot be the same exact word in the list.
+     * @param someList List
+     * @return List<String></String>
+     */
     public List<String> match(List<String> someList)
     {
+        char[] objectWord = this.aWord.toCharArray();
+        Arrays.sort(objectWord);
+
         List<String> answer = new ArrayList<>();
+        HashMap<String, Integer> mapForEachWord = new HashMap<>();
+
+        for (int i = 0; i < someList.size(); i++)
+        {
+            String wordFromList = someList.get(i).toLowerCase();
+            //mapForEachWord = processWord(wordFromList);
+
+            char[] listWordArray = wordFromList.toCharArray();
+            Arrays.sort(listWordArray);
+
+            if (Arrays.equals(listWordArray, objectWord) && !this.aWord.equals(wordFromList))
+            {
+                answer.add(someList.get(i));
+            }
+
+        }
+
+        return answer;
     }
 
     public static HashMap<String, Integer> processWord(String aWord)
