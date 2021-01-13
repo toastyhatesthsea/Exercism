9d5541a09c120276703315036a02b7d45c86b8a9
toastyhatesthsea <toastyhatesthesea@gmail.com>
2021-01-12 23:22:54 -0800

    Added initial code for Pascal

---
+++ PascalsTriangleGenerator.java
@@ -0,0 +1,51 @@
+/*
+
+Since this exercise has a difficulty of > 4 it doesn't come
+with any starter implementation.
+This is so that you get to practice creating classes and methods
+which is an important part of programming in Java.
+
+Please remove this comment when submitting your solution.
+
+*/
+
+
+public class PascalsTriangleGenerator
+{
+    public int[][] generateTriangle(int sizeOfTriangle)
+    {
+        int[][] answer = new int[sizeOfTriangle][sizeOfTriangle];
+
+        for (int i = 0; i < sizeOfTriangle; i++)
+        {
+            if (sizeOfTriangle == 1)
+            {
+                answer[0][0] = 1;
+            }
+        }
+
+
+        return answer;
+    }
+
+    public int[] createArrayRow(int[] previousAboveArrayInTriangle)
+    {
+        int[] answer = new int[previousAboveArrayInTriangle.length + 1];
+        for (int i = 0; i < answer.length; i++)
+        {
+            if (i == 0)
+            {
+                answer[i] = 1;
+            } else if (i == answer.length - 1)
+            {
+                answer[i] = 1;
+            }
+            else
+            {
+                int firstSlot = previousAboveArrayInTriangle[i - 1] + previousAboveArrayInTriangle[i];
+                answer[i] = firstSlot;
+            }
+        }
+        return answer;
+    }
+}
